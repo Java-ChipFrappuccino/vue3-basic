4a8e3ad9e1e4a227b7ce87295fd16335057701ef
Java-ChipFrappuccino <pss21242124@gmail.com>
2024-12-27 00:04:33 +0900

    24.12.27

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,19 @@
-<script setup></script>
+<script setup>
+import { reactive } from 'vue';
+
+export default {
+  setup() {
+    const original = reactive({
+      count: 0,
+    });
+    original.count++;
+    console.log(original.count)
+    return {
+
+    };
+  },
+}
+</script>
 
 <template>
   <div></div>
